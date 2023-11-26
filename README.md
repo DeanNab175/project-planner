b95412672777cce7ff95501be5a16d29fd945c86
DeanNab175 <f.jscript@gmail.com>
2023-11-26 13:39:23 +0400

    project setup, add json-server and project planner data

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# project-planner
-A vuejs project planner app
+# Project Planner
+
+A vue 3 project planner app.
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
---
+++ src/views/HomeView.vue
@@ -0,0 +1,29 @@
+<template>
+  <main>
+    <div v-if="projects.length">
+      <div v-for="project in projects" :key="project.id">
+        <SingleProject :project="project" />
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+import SingleProject from "../components/SingleProject.vue";
+
+export default {
+  name: "Home",
+  components: { SingleProject },
+  data() {
+    return {
+      projects: [],
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/projects")
+      .then((res) => res.json())
+      .then((data) => (this.projects = data))
+      .catch((err) => console.log(err.message));
+  },
+};
+</script>
